78103f6839cc9e3fce39216bbf858e289f5cd3fa
Cash-Eze <amoshiamatsu@gmail.com>
2023-11-29 03:28:26 -0800

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/ python3
+"""Writng a function Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """
+        Printing the Triangle
+    """
+    trow = []
+    x = [0]
+    if n <= 0:
+        return []
+    else:
+        for i in range(n):
+            print(trow)
+            trow = [left+right, for left, right in zip(trow + x, x + trow)]
