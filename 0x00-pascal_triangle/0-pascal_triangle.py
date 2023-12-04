16e1264cf2b2517cff25c8715b1444f7b67f2732
Cash-Eze <amoshiamatsu@gmail.com>
2023-12-04 01:30:05 -0800

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Writng a function Pascal Triangle"""
+'''A module for working with Pascal's triangle.
+'''
 
 
 def pascal_triangle(n):
-    """
-        Printing the Triangle
-    """
-    trow = []
-    x = [0]
-    if n <= 0:
-        return []
-    else:
-        for i in range(n):
-            print(trow)
-            trow = [left+right, for left, right in zip(trow + x, x + trow)]
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
